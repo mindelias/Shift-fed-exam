8b49186307d2851228ebba56594b9af38c860e24
mindelias <mindelias@gmail.com>
2025-06-13 12:23:20 +0100

    Part 1a - display issue description

--- inertia/pages/index.tsx
+++ inertia/pages/index.tsx
@@ -31,6 +31,11 @@ function TicketsList({ tickets }: { tickets: Ticket[] }) {
           className="bg-white border border-sand-7 rounded-lg p-6 hover:border-sand-8 hover:shadow-sm transition duration-200"
         >
           <h5 className="text-lg font-semibold text-sand-12 mb-2">{ticket.title}</h5>
+
+          {/* added description */}
+
+          <p className="text-sm text-sand-11 whitespace-pre-line mb-4">{ticket.content}</p>
+
           <footer>
             <div className="text-sm text-sand-10">
               By {ticket.userEmail} | {formatDate(ticket.creationTime)}
